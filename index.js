11855d321ab77ce535b5fd69059c6c34d43af588
admin <admin@admins-MacBook-Pro.local>
2019-11-05 15:16:41 -0800

    mvp complete database and api working at port 7000

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const server = require('./server.js')
+
+const port = process.env.PORT || 7000;
+
+server.listen(port, () => {
+     console.log(`Listening to ${port}`)
+})
